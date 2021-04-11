cc107867f8b12c0a2a1e6d95ab6c3abf40e82b09
Shirley Chen <stasl0217@gmail.com>
2021-04-10 23:11:23 -0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
 
 """
 Working directory: project root
@@ -33,17 +31,11 @@ import src.param as param
 from src.model import save_model_structure, create_alignment_kNN_finder
 from src.data_loader import load_support_kgs, load_target_kg, load_align_link_table, take_seed_align_links,     load_all_to_all_seed_align_links
 from src.validate import MultiModelTester, TestMode, test_alignment_hits10
-from src.modellist import AlignModel
 import numpy as np
 import logging
-from src.validate import filt_hits_at_n, filt_ensemble_hits_at_n
-import ast
-from collections import defaultdict
-from scipy.spatial.distance import cosine as cos_dist
 from src.model import extend_seed_align_links
+import argparse
 
-
-# In[4]:
 
 
 def set_logger(param, model_dir):
@@ -67,149 +59,182 @@ def set_logger(param, model_dir):
     logging.getLogger('').addHandler(console)
 
 
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='Training and Testing Knowledge Graph Embedding Models',
+        usage='run.py [<args>] [-h | --help]'
+    )
+    parser.add_argument('-l', '--target_language', type=str, choices=['ja', 'el', 'es', 'fr', 'en'], help="target kg")
+    parser.add_argument('-m', '--knowledge_model', default='transe', type=str, choices=['transe', 'rotate'])
+    parser.add_argument('--use_default', action="store_true", help="Use default setting. This will override every setting except for targe_langauge and knowledge_model")
+    parser.add_argument('-lr', '--learning_rate', default=1e-2, type=float, help="learning rate for knowledge model")
+    parser.add_argument('--align_lr', default=1e-3, type=float, help="learning rate for knowledge model")
+    parser.add_argument('-b', '--batch_size', default=1024, type=int, help="batch size of queries")
+    parser.add_argument('-d', '--dim', default=400, type=int)
+    parser.add_argument('--transe_margin', default=0.3, type=float)
+    parser.add_argument('--rotate_gamma', default=24, type=float)
+    parser.add_argument('--reg_scale', default=1e-5, type=float, help="scale for regularization")
+    parser.add_argument('--base_align_step', default=5, type=float, help="how many align model epoch to train before switching to knowledge model")
+    parser.add_argument('--knowledge_step_ratio', default=2, type=float, help="how many knowledge model epochs for each align epoch")
+    parser.add_argument('--round', default=5, type=float,
+                        help="how many rounds to train")
+    parser.add_argument('-k', default=10, type=int, help="how many nominations to consider")
 
-param.lang = 'ja'
+    return parser.parse_args(args)
+
+def set_params(args):
+    param.lang = args.target_language
+    param.knowledge = args.knowledge_model
+
+    if args.use_default:
+        if param.knowledge == 'transe':
+            param.epoch10 = 10
+            param.epoch11 = 10
+            param.epoch2 = 5
+            param.lr = 1e-3
+            param.dim = 300
+            param.round = 2
+        elif param.knowledge == 'rotate':
+            param.epoch10 = 100
+            param.epoch11 = 100
+            param.epoch2 = 5
+            param.lr = 1e-2
+            param.dim = 400
+            param.round = 3
+    else:
+        param.dim = args.dim
+        param.lr = args.learning_rate
+        param.batch_size = args.batch_size
+        param.epoch2 = args.base_align_step
+        param.epoch10 = param.epoch11 = args.base_align_step * args.knowledge_step_ratio
+        param.gamma = args.rotate_gamma
+        param.margin = args.transe_margin
+        param.reg_scale = args.reg_scale
+        param.round = args.round
+
+def main(args):
+    set_params(args)
+
+    target_lang = param.lang
+    src_langs = ['fr', 'ja', 'es', 'el', 'en']
+    src_langs.remove(target_lang)
+
+    target_lang = param.lang
+
+    # load data
+    data_dir = './data/kg'  # where you put kg data
+    seed_dir = './data/seed_alignlinks'  # where you put seed align links data
+    model_dir = join('./trained_model', f'kens-{param.knowledge}-{param.dim}', target_lang)  # output
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+    # logging
+    set_logger(param, model_dir)  # set logger
+    logging.info('Knowledge model: %s'%(param.knowledge))
+    logging.info('target language: %s'%(param.lang))
+
+    # hyper-parameters
+    logging.info(f'dim: {param.dim}')
+    logging.info(f'lr: {param.lr}')
 
 
-param.epoch10 = 100
-param.epoch11 = 100
-param.epoch2 = 5
-param.lr = 0.01
-param.dim = 1000
-param.round = 5
-param.align_lr = 1e-4
-# param.verbose = args.verbose
-param.align_model = AlignModel('same')
-param.updating_embedding = False
-param.margin = 0.3
-
-param.val_freq = 1
-
-
-target_lang = param.lang
-src_langs = ['fr', 'en', 'es',  'ja',  'el']
-src_langs.remove(target_lang)
-
-
-
-# load data
-data_dir = './data/kg'  # where you put kg data
-seed_dir = './data/seed_alignlinks'  # where you put seed align links data
-model_dir = join('./model', f'kens-{param.knowledge}', target_lang)  # output
-if not os.path.exists(model_dir):
-    os.makedirs(model_dir)
-
-    
-# logging
-set_logger(param, model_dir)  # set logger
-logging.info('Knowledge model: %s'%(param.knowledge))
-logging.info('target language: %s'%(param.lang))
-             
-# hyper-parameters
-logging.info(f'dim: {param.dim}')
-logging.info(f'lr: {param.lr}')
-logging.info('margin: %.2f'%param.margin)
-
-
-# target (sparse) kg
-kg0 = load_target_kg(data_dir, target_lang, testfile_suffix='-val.tsv')  # target kg. KnowledgeGraph object
-# supporter kgs
-supporter_kgs = load_support_kgs(data_dir, seed_dir, target_lang, src_langs)  # list[KnowledgeGraph]
-
-# seed alignment links
-seed_alignlinks = load_all_to_all_seed_align_links(seed_dir)  # {(lang1, lang2): 2-col np.array}
-
-all_kgs = [kg0] + supporter_kgs
-
-# build alignment model (all-to-all)
-for kg in all_kgs:
-    kg.build_alignment_models(all_kgs)  # kg.align_models_of_all {lang: align_model}
-
-# create validator
-validator = MultiModelTester(kg0, supporter_kgs)
-
-print('model initialization done')
-
-
-for i in range(param.round):
-    
-    # train alignment model
-    for kg in all_kgs:
-        # align it with everything else
-        for other_lang, align_model in kg.align_models_of_all.items():
-            if (other_lang, kg.lang) in seed_alignlinks:  # seed_alignlinks {(lang1, lang2): 2-col np.array}
-                # use if to avoid retrain the same pair of languages
-                align_links = seed_alignlinks[(other_lang, kg.lang)]
-                align_model.fit([align_links[:, 0], align_links[:, 1]], np.zeros(align_links.shape[0]),
-                                epochs=param.epoch2, batch_size=param.batch_size, shuffle=True, )
-
-    # # # self-learning
-    # for kg in all_kgs:
-    #     for other_kg in all_kgs:
-    #         if other_kg.lang != kg.lang and (other_kg.lang, kg.lang) in seed_alignlinks:
-    #             print(f'self learning[{kg.lang}][{other_kg.lang}]')
-    #             seeds = seed_alignlinks[(other_kg.lang, kg.lang)]
-    #             found = extend_seed_align_links(other_kg, kg, seeds)
-    #             if len(found) > 0:  # not []
-    #                 new_seeds = np.concatenate([seeds, found], axis=0)
-    #                 seed_alignlinks[(other_kg.lang, kg.lang)] = new_seeds
-
-    # train knowledge model
-    kg0.model.fit([kg0.h_train, kg0.r_train, kg0.t_train], kg0.y_train,
-                  epochs=param.epoch10, batch_size=param.batch_size, shuffle=True, )
+    # target (sparse) kg
+    kg0 = load_target_kg(data_dir, target_lang, testfile_suffix='-val.tsv')  # target kg. KnowledgeGraph object
+    # supporter kgs
+    supporter_kgs = load_support_kgs(data_dir, seed_dir, target_lang, src_langs)  # list[KnowledgeGraph]
+    # supporter KG use all links to train
     for kg1 in supporter_kgs:
-        kg1.model.fit([kg1.h_train, kg1.r_train, kg1.t_train], kg1.y_train,
-                      epochs=param.epoch11, batch_size=param.batch_size, shuffle=True, )
+        kg1.h_train = np.concatenate([kg1.h_train, kg1.h_test], axis=0)
+        kg1.r_train = np.concatenate([kg1.r_train, kg1.r_test], axis=0)
+        kg1.t_train = np.concatenate([kg1.t_train, kg1.t_test], axis=0)
+        kg1.y_train = np.zeros(kg1.h_train.shape[0])
 
-    model_weights = []
-    if i % param.val_freq == 0:  # validation
-        param.updating_embedding = False
+    # seed alignment links
+    seed_alignlinks = load_all_to_all_seed_align_links(seed_dir)  # {(lang1, lang2): 2-col np.array}
 
-        logging.info(f'=== round {i}')
-        logging.info(f'[{kg0.lang}]')
-        model_weights = []
-        hits10_kg0 = validator.test(TestMode.KG0)
-        model_weights.append(hits10_kg0)
+    all_kgs = [kg0] + supporter_kgs
 
+    # build alignment model (all-to-all)
+    for kg in all_kgs:
+        kg.build_alignment_models(all_kgs)  # kg.align_models_of_all {lang: align_model}
+
+    # create validator
+    validator = MultiModelTester(kg0, supporter_kgs)
+
+    print('model initialization done')
+
+    for i in range(param.round):
+        # train alignment model
+        for kg in all_kgs:
+            # align it with everything else
+            for other_lang, align_model in kg.align_models_of_all.items():
+                if (other_lang, kg.lang) in seed_alignlinks:  # seed_alignlinks {(lang1, lang2): 2-col np.array}
+                    # use if to avoid retrain the same pair of languages
+                    align_links = seed_alignlinks[(other_lang, kg.lang)]
+                    align_model.fit([align_links[:, 0], align_links[:, 1]], np.zeros(align_links.shape[0]),
+                                    epochs=param.epoch2, batch_size=param.batch_size, shuffle=True, )
+
+        # self-learning
+        for kg in all_kgs:
+            for other_kg in all_kgs:
+                if other_kg.lang != kg.lang and (other_kg.lang, kg.lang) in seed_alignlinks:
+                    print(f'self learning[{kg.lang}][{other_kg.lang}]')
+                    seeds = seed_alignlinks[(other_kg.lang, kg.lang)]
+                    found = extend_seed_align_links(other_kg, kg, seeds)
+                    if len(found) > 0:  # not []
+                        new_seeds = np.concatenate([seeds, found], axis=0)
+                        seed_alignlinks[(other_kg.lang, kg.lang)] = new_seeds
+
+        # train knowledge model
+        kg0.model.fit([kg0.h_train, kg0.r_train, kg0.t_train], kg0.y_train,
+                      epochs=param.epoch10, batch_size=param.batch_size, shuffle=True, )
         for kg1 in supporter_kgs:
-            logging.info(f'[{kg1.lang}]')
-            hits10 = validator.test(TestMode.KG1, supporter_kg=kg1)
-            model_weights.append(hits10)
-        print(model_weights)
+            kg1.model.fit([kg1.h_train, kg1.r_train, kg1.t_train], kg1.y_train,
+                          epochs=param.epoch11, batch_size=param.batch_size, shuffle=True, )
+
+        model_weights = []
+        if i % param.val_freq == 0:  # validation
+            param.updating_embedding = False
+
+            logging.info(f'=== round {i}')
+            logging.info(f'[{kg0.lang}]')
+            hits10_kg0 = validator.test(TestMode.KG0)
 
 
-kg0.save_model(model_dir)
-for kg1 in supporter_kgs:
-    kg1.save_model(model_dir)
+    kg0.save_model(model_dir)
+    for kg1 in supporter_kgs:
+        kg1.save_model(model_dir)
 
-save_model_structure(kg0.kNN_finder, os.path.join(model_dir, 'kNN_finder.json'))
+    save_model_structure(kg0.kNN_finder, os.path.join(model_dir, 'kNN_finder.json'))
 
-# Populate the alignment set using newly predicted links
-for kg1 in supporter_kgs:
-    kg1.populate_alignlinks(kg0, seed_alignlinks)
+    for kg1 in supporter_kgs:
+        kg1.populate_alignlinks(kg0, seed_alignlinks)
 
-choices = ['-val.tsv', '-test.tsv']  # '-val.tsv' if predict on validation data, '-test.tsv' if on test data
-validator = MultiModelTester(kg0, supporter_kgs)
+    choices = ['-val.tsv', '-test.tsv']  # '-val.tsv' if predict on validation data, '-test.tsv' if on test data
+    validator = MultiModelTester(kg0, supporter_kgs)
 
-kg0.filtered_reordered_embedding_matrix = None
-for kg1 in supporter_kgs:
-    kg1.filtered_reordered_embedding_matrix = None
+    kg0.filtered_reordered_embedding_matrix = None
+    for kg1 in supporter_kgs:
+        kg1.filtered_reordered_embedding_matrix = None
 
-val_df = None
-test_df = None
+    val_df = None
+    test_df = None
 
-for suffix in choices:
-    testfile = join(data_dir, target_lang + suffix)
-    output = join(model_dir, 'results'+suffix)
-    testcases = pd.read_csv(testfile, sep='\t', header=None).values
-    param.n_test = testcases.shape[0]  # test on all training triples
-    print('Loaded test cases from: %s'%testfile)
+    for suffix in choices:
+        testfile = join(data_dir, target_lang + suffix)
+        output = join(model_dir, 'results'+suffix)
+        testcases = pd.read_csv(testfile, sep='\t', header=None).values
+        param.n_test = testcases.shape[0]  # test on all training triples
+        print('Loaded test cases from: %s'%testfile)
 
-    results_df = validator.test_and_record_results(testcases)
-    if suffix == '-val.tsv':
-        val_df = results_df
-    elif suffix == '-test.tsv':
-        test_df = results_df
+        results_df = validator.test_and_record_results(testcases)
+        if suffix == '-val.tsv':
+            val_df = results_df
+        elif suffix == '-test.tsv':
+            test_df = results_df
 
-    results_df.to_csv(join(output), sep='\t', index=False)
+        results_df.to_csv(join(output), sep='\t', index=False)
+
+if __name__ == "__main__":
+    main(parse_args())
 
--- test.py
+++ test.py
@@ -21,160 +21,13 @@ import logging
 import pandas as pd
 import ast
 import time
-from pandarallel import pandarallel
-from src.validate import MultiModelTester, filt_hits_at_n, filt_ensemble_hits_at_n, hits10_with_weight, hits3_with_weight, hits1_with_weight
+# from pandarallel import pandarallel
+from src.validate import MultiModelTester, filt_hits_at_n, filt_ensemble_hits_at_n, hr2t_from_train_set
 from src.ensemble import *
 from src.data_loader import *
+from src.weightlearning import *
+import argparse
 
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-def weight_matrix_from_one_triple(row):
-    # weight_matrix[i] is the datapoint weight for the entity pair (true_t, i)
-    weight_matrix = np.ones(num_entity)
-    return weight_matrix
-
-
-def rank_matrix_from_one_triple(row):
-    """
-    rank_matrix shape: [num_entity, len(langs)]
-    rank_matrix[i,j]=1 means entity[i] is correctly ranked after true_t by lang j, -1 means wrongly
-    rank_matrix[true_t, true_t] = 1
-    :param row: a row in df
-    :return:
-    """
-    true_t = row['t']
-
-    rank_matrix = 1 * np.ones(
-        [num_entity, len(langs)])  # all initialized as -1 (ranked wrong by default) excpet for true_t
-
-    for j, lang in enumerate(langs):
-        predictions = row[lang]
-
-        ths = int(100 / mrrs[target_lang][lang])
-        if true_t in predictions:
-            # unseen datapoints as ranked correctly
-            rank_matrix[:, j] = 1
-            rank = predictions.index(true_t)  # start from 0
-            if rank > 0:
-                # update rank_matrix for entities before true_t (they are ranked wrongly)
-                rank_matrix[np.array(predictions[:rank + 1]), j] = -1
-        else:
-            # assume it's ranked around mean rank
-            rank_matrix[np.array(predictions).astype(np.int32), j] = -1  # entities in predictions are wrongly ranked
-            # sample the rest
-            rest = np.random.randint(0, num_entity, size=(ths - len(predictions)))
-            rank_matrix[rest, j] = -1
-    return rank_matrix
-
-
-# In[ ]:
-
-
-def triples_of_given_entity(df, e):
-    return df[(df['h'] == e) | (df['t'] == e)]
-
-
-#     return df[(df['h'] == e)]
-
-
-def model_weights_for_one_round(weight_matrix, rank_matrix):
-    # learn model weights
-    # wrong[i,j] = -a means language j wrongly ranks entity i before the true_t,
-    # and the pair (true_t, i) has weight a
-    wrong = np.zeros(rank_matrix.shape, dtype=np.float32)
-    for j in range(len(langs)):  # copy weights for wrong predictions
-        row_indices = (rank_matrix[:, j] == -1)
-        wrong[row_indices, j] = weight_matrix[row_indices]
-    wrong = np.sum(wrong, axis=0) + 1e-7  # to make it non-zero
-
-    correct = np.zeros(rank_matrix.shape)
-    for j in range(len(langs)):
-        row_indices = (rank_matrix[:, j] == 1)
-        correct[row_indices, j] = weight_matrix[row_indices]
-    correct = np.sum(correct, axis=0) + 1e-7
-
-    lang_weights = 0.5 * np.log(correct / wrong)
-
-    return lang_weights
-
-
-def learn_model_weights(weight_matrix, rank_matrix):
-    T = 5
-    lang_final_weights = np.zeros(len(langs))
-
-    for i in range(T):
-        lang_weights = model_weights_for_one_round(weight_matrix, rank_matrix)
-
-        # choose language (best of this round) that has not been chosen
-        lang_order = np.argsort(lang_weights)[
-                     ::-1]  # lang_order[k]=j means language j is the kth best (has kth largest weight)
-
-        for lang in lang_order:
-            if not lang_final_weights[lang] > 0:
-                best_lang = lang
-                break
-        best_lang_weight = lang_weights[best_lang]
-        lang_final_weights[best_lang] += best_lang_weight
-
-        # update datapoint weights according to prediction of the best language
-        coeff = np.exp(-best_lang_weight * rank_matrix[:, best_lang])
-        weight_matrix = weight_matrix * coeff
-        weight_matrix = weight_matrix / np.sum(weight_matrix)  # normalize
-
-    final = {lang: w for lang, w in zip(langs, lang_final_weights)}
-
-    return final
-
-
-def learn_entity_specific_weights(target_lang, df, e):
-    if target_lang == 'en':
-        MIN_TRIPLE = 1
-    else:
-        MIN_TRIPLE = 3
-
-
-    triples = triples_of_given_entity(df, e)
-    if len(triples) < MIN_TRIPLE:  # too few triples, just use mrr
-        return mrrs[target_lang]
-
-    subdf = triples.copy().reset_index(drop=True)
-
-    # get entity pair weights and prediction correctness
-    subdf['weight_matrix'] = ''
-    subdf['weight_matrix'] = subdf.apply(lambda row: weight_matrix_from_one_triple(row), axis=1)
-    subdf['rank_matrix'] = subdf.apply(lambda row: rank_matrix_from_one_triple(row), axis=1)
-
-    # concatenate matrices for multiple triples by axis=0
-    big_weight_matrix = np.concatenate(subdf['weight_matrix'].values, axis=0)
-    big_rank_matrix = np.concatenate(subdf['rank_matrix'].values, axis=0)
-    # normalize datapoint weights
-    big_weight_matrix = big_weight_matrix / np.sum(big_weight_matrix)
-
-    model_weights = learn_model_weights(big_weight_matrix, big_rank_matrix)
-    return model_weights
-
-
-# when no related triple in validation set, use MRR by default
-if param.knowledge == 'transe':
-    mrrs = {'el': {'el': 0.463, 'fr': 0.538, 'en': 0.359, 'es': 0.478, 'ja': 0.67},
-            'ja': {'ja': 0.385, 'fr': 0.569, 'en': 0.377, 'es': 0.435, 'el': 0.75},
-            'es': {'es': 0.425, 'fr': 0.54, 'en': 0.352, 'ja': 0.685, 'el': 0.764},
-            'fr': {'fr': 0.359, 'en': 0.38, 'es': 0.459, 'ja': 0.70, 'el': 0.77},
-            'en': {'en': 0.297, 'fr': 0.489, 'es': 0.50, 'ja': 0.675, 'el': 0.758}
-            }
-else:
-    mrrs = {
-        'en': {'en': 0.37, 'fr': 0.648, 'es': 0.657, 'ja': 0.722, 'el': 0.836},
-        'fr': {'ja': 0.34, 'fr': 0.67, 'en': 0.57, 'es': 0.68, 'el': 0.80},
-        'ja': {'ja': 0.34, 'fr': 0.67, 'en': 0.57, 'es': 0.68, 'el': 0.80},
-        'el': {'el': 0.11, 'fr': 0.74, 'en': 0.55, 'es': 0.71, 'ja': 0.71},
-        'es': {'es': 0.43, 'fr': 0.684, 'en': 0.573, 'ja': 0.717, 'el': 0.804}
-    }
-
-
-langs = [ 'fr', 'en', 'es',  'ja',  'el']
-#%%%%%%%%%%%%%%%%%%%%%%
 
 
 def set_logger(param, model_dir):
@@ -198,153 +51,76 @@ def set_logger(param, model_dir):
     logging.getLogger('').addHandler(console)
 
 
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description='Training and Testing Knowledge Graph Embedding Models',
+        usage='test.py [<args>] [-h | --help]'
+    )
+    parser.add_argument('-l', '--target_language', type=str, choices=['ja', 'el', 'es', 'fr', 'en'], help="target kg")
+    parser.add_argument('-m', '--knowledge_model', default='transe', type=str, choices=['transe', 'rotate'])
+    parser.add_argument('--model_dir', type=str, help="Directory of trained model")
+    parser.add_argument('-d', '--dim', type=int, help="Dimension of the saved model")
+
+    return parser.parse_args(args)
 
 
-def run(lang):
-    target_lang = lang
-    src_langs = ['fr', 'ja', 'es', 'el', 'en']
+def main(args):
+    target_lang = args.target_language
+    param.knowledge = args.knowledge_model
+    model_dir = args.model_dir
+
+    param.lang = target_lang
+
+    src_langs = ['fr', 'ja', 'es',  'el',  'en']
     src_langs.remove(target_lang)
+    # load data
+    data_dir = './data/kg'
+    entity_dir = './data/entity'
+    vocabs = pd.read_csv(join(entity_dir, f'{target_lang}.tsv'), sep='\t', header=None)
+    num_entity = len(vocabs)
+    hr2t_train = hr2t_from_train_set(data_dir, target_lang)  # used for filtered test setting
 
-    suffix = '-val.tsv'  # use validation file for weight learning
-    testfile = join(data_dir, target_lang + suffix)
-
-    set_logger(param, model_dir)
+    # logging
+    set_logger(param, model_dir)  # set logger.
+    logging.info('=== Boosting')
     logging.info(f'target_lang: {target_lang}')
 
-
-    try:
-        # target (sparse) kg
-        kg0 = load_target_kg(data_dir, target_lang)  # target kg. KnowledgeGraph object
-        # supporter kgs
-        supporter_kgs = load_support_kgs(data_dir, seed_dir, target_lang, src_langs)  # list[KnowledgeGraph]
-    except ValueError:
-        print(f'{lang} skipped')
-
-    validator = MultiModelTester(kg0, supporter_kgs)
-
-    # load model
-    kg0.load_model(model_dir)
-    for kg1 in supporter_kgs:
-        kg1.load_model(model_dir)
-
-    # recompute filtered_reordered_embedding_matrix using the latest embeddings
-    kg0.filtered_reordered_embedding_matrix = None
-    for kg1 in supporter_kgs:
-        kg1.filtered_reordered_embedding_matrix = None
+    langs = [target_lang] + src_langs
 
 
-    output = join(model_dir, 'results'+suffix)
-    testcases = pd.read_csv(testfile, sep='\t', header=None).values
-    param.n_test = testcases.shape[0]  # test on all training triples
-    print('Loaded test cases from: %s'%testfile)
+    # load predictions on validation set
+    df = pd.read_csv('results-val.tsv', sep='\t')  # (h,r,t) => predictions
+    for model in langs:  # convert string to list
+        df[model] = df[model].apply(lambda x: ast.literal_eval(x))
+        # extract entities
+        df[model] = df[model].apply(lambda x: extract_entities(x))
 
-    results_df = validator.test_and_record_results(testcases)
-    results_df.to_csv(output, sep='\t', index=False)
+    # load predictions on test set for testing
+    test_result_file = join(model_dir,  'results-test.tsv')
+    result = pd.read_csv(test_result_file, sep='\t')
+    for lang in langs:
+        result[lang] = result[lang].apply(lambda x: ast.literal_eval(x))  # convert list string to list
+    print(result.head(2))
 
+    # learn weights for all entities
+    weight_df = pd.DataFrame(np.arange(num_entity), columns=['e'])
+    base = {lang: filt_hits_at_n(result, lang, hr2t_train, n=10) for lang in langs}
+    weight_df['weights'] = weight_df['e'].apply(lambda e: learn_entity_specific_weights(target_lang, langs, df, e, num_entity,base))
 
-target_lang = 'ja'
+    wdf = weight_df.copy()
+    weights = pd.Series(wdf['weights'].values, index=wdf['e']).to_dict() # {entity: {model:weight}}
+    wdf.to_csv(join(model_dir, 'weights.tsv'), sep='\t', index=False)
 
-param.lang = target_lang
+    logging.info(f'[{target_lang}]')
 
-src_langs = ['fr', 'ja', 'es',  'el',  'en']
-src_langs.remove(target_lang)
-
-
-
-
-# load data
-data_dir = './data/kg'
-seed_dir = './data/seed_alignlinks'
-model_dir = join('./model', f'kens-{param.knowledge}', target_lang)  # output
-
-output_file = join(model_dir, 'weights.tsv')
-
-entity_dir = './data/entity'
-vocabs = pd.read_csv(join(entity_dir, f'{target_lang}.tsv'), sep='\t', header=None)
-num_entity = len(vocabs)
-
-# logging
-set_logger(param, model_dir)  # set logger.
-logging.info('=== Boosting')
-logging.info(f'target_lang: {target_lang}')
+    logging.info('=== boosting')
+    filt_ensemble_hits_at_n(result, weights, langs, hr2t_train, n=10)
+    filt_ensemble_hits_at_n(result, weights, langs, hr2t_train, n=3)
+    filt_ensemble_hits_at_n(result, weights, langs, hr2t_train, n=1)
 
 
-test_result_file = join(model_dir,  'results-test.tsv')
-weights_file = join(model_dir,'weights.tsv')
-langs = [target_lang] + src_langs
-# === PARAMETERS END ===
-
-
-
-def extract_entities(id_score_tuples):
-    return [ent_id for ent_id, score in id_score_tuples]
-
-langs =[ 'fr', 'en', 'es',  'ja',  'el']
-
-# use validation set predictions for weight learning
-file = join(model_dir, 'results-val.tsv')  # if not generated, change the suffix in train.ipynb and generate it
-output = join(model_dir, 'weights.tsv')
-M = len(langs)
-
-pandarallel.initialize()
-
-# if os.path.exists(join(model_dir, 'results-val.tsv')):
-#     run('ja')
-
-# load predictions on validation set
-df = pd.read_csv(file, sep='\t')  # (h,r,t) => predictions
-for model in langs:  # convert string to list
-    df[model] = df[model].apply(lambda x: ast.literal_eval(x))
-    # extract entities
-    df[model] = df[model].apply(lambda x: extract_entities(x))
-    
-
-# load predictions on test set for testing
-test_result_file = join(model_dir,  'results-test.tsv')
-result = pd.read_csv(test_result_file, sep='\t')
-# result = result.loc[:1000,:]
-for lang in langs:
-    result[lang] = result[lang].apply(lambda x: ast.literal_eval(x))  # convert list string to list
-print(result.head(2))
-
-
-
-# multiprocessing weight learning
-time0 = time.time()
-
-# learn weights for all entities
-weight_df = pd.DataFrame(np.arange(num_entity), columns=['e'])
-weight_df['weights'] = weight_df['e'].parallel_apply(lambda e: learn_entity_specific_weights(target_lang, df, e))
-
-print(f'time {time.time()-time0}')
-
-print('# of entities:', len(weight_df))
-weight_df.head(5)
-
-
-def hr2t_from_train_set():
-    train_df = pd.read_csv(join(data_dir, f'{target_lang}-train.tsv'), sep='\t')
-    tripleset = set([tuple([h,r,t]) for h,r,t in (train_df.values)])
-
-    hr2t = {}  # {(h,r):set(t)}
-    for tp in tripleset:
-        h,r,t=tp[0],tp[1],tp[2]
-        if (h,r) not in hr2t:
-            hr2t[(h,r)] = set()
-        hr2t[(h,r)].add(t)
-    return hr2t
-
-
-wdf = weight_df.copy()
-weights = pd.Series(wdf['weights'].values, index=wdf['e']).to_dict() # {entity: {model:weight}}
-
-logging.info(f'[{target_lang}]')
-hr2t_train = hr2t_from_train_set()
-
-logging.info('-- filtered')
-filt_ensemble_hits_at_n(result, weights, langs, hr2t_train, n=10)
-filt_ensemble_hits_at_n(result, weights, langs, hr2t_train, n=3)
-filt_ensemble_hits_at_n(result, weights, langs, hr2t_train, n=1)
+if __name__ == "__main__":
+    main(parse_args())
 
 
 
